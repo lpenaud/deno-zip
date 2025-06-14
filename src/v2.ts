a512c594d2423a63f5c9ea4fc3ce34baf2c80899
lpenaud <l.penaud@zaclys.net>
2025-06-14 08:36:48 +0200

    FIX: It's work

--- src/v2.ts
+++ src/v2.ts
@@ -1,194 +1,252 @@
 import { startsWith } from "@std/bytes";
-import {
-  Compression,
-  EMPTY_UINT8_ARRAY,
-  LOCAL_FILE_SIGNATURE,
-} from "./constants.ts";
+import { Compression, LOCAL_FILE_SIGNATURE } from "./constants.ts";
 import * as stdPath from "@std/path";
 import * as stdFs from "@std/fs";
-import * as stdStreams from "@std/streams";
 
-class ByobTransformStream {
-
-  #views: Uint8Array[];
-
-  #reader: ReadableStreamDefaultReader<Uint8Array>;
-
-  #byteLength: number;
-
-  constructor(readable: ReadableStream<Uint8Array>) {
-    this.#reader = readable.getReader();
-    this.#views = [];
-    this.#byteLength = 0;
-  }
-
-  async read(n: number): Promise<Uint8Array> {
-    let r = new Uint8Array(n);
-    if (n === 0) {
-      return r;
-    }
-    while (this.#byteLength <= n) {
-      const { done, value } = await this.#reader.read();
-      if (value) {
-        this.#views.push(value.slice());
-        this.#byteLength += value.byteLength;
-      }
-      if (done) {
-        break;
-      }
-    }
-    let i: number;
-    for (i = 0; i < this.#views.length; i++) {
-      const v = this.#views[i];
-      const copied = v.subarray(0, Math.min(r.byteLength, v.byteLength));
-      r.set(copied);
-      r = r.subarray(copied.byteLength);
-      if (r.byteLength === 0) {
-        if (copied.byteLength !== v.byteLength) {
-          this.#views[i] = v.subarray(copied.byteLength);
-        } else {
-          i++;
-        }
-        break;
-      }
-    }
-    this.#views.splice(0, i);
-    this.#byteLength -= n;
-    return new Uint8Array(r.buffer);
-  }
-
-  cancel(reason?: string): Promise<void> {
-    return this.#reader.cancel(reason);
-  }
-}
-
-interface LocalFileHeader {
+interface InternalLocalFileHeader {
   version: number;
   flags: number;
   compression: number;
-  mode: [number, number];
-  crc32: number;
+  crc32: Uint8Array;
   compressedSize: number;
   size: number;
   filenameLength: number;
   extraFieldsLength: number;
+  date: Date | undefined;
 }
 
-function createHeader(buffer: Uint8Array): LocalFileHeader {
-  const view = new DataView(
-    buffer.buffer,
-    buffer.byteOffset + LOCAL_FILE_SIGNATURE.byteLength,
-  );
-  return {
-    version: view.getUint16(0, true),
-    flags: view.getUint16(2, true),
-    compression: view.getUint16(4, true),
-    mode: [view.getUint16(8, true), view.getUint16(6, true)],
-    crc32: view.getUint32(10, true),
-    compressedSize: view.getUint32(14, true),
-    size: view.getUint32(18, true),
-    filenameLength: view.getUint16(22, true),
-    extraFieldsLength: view.getUint16(24, true),
-  };
+interface ZipEntryHeader {
+  version: number;
+  flags: number;
+  compression: number;
+  date: Date | undefined;
+  crc32: number;
+  compressedSize: number;
+  size: number;
+  filename: string;
+  extraFields: [number, number][];
 }
 
 interface ZipEntry {
-  headerBuffer: Uint8Array;
-  header: LocalFileHeader;
-  filename: string;
+  header: ZipEntryHeader;
   readable: ReadableStream<Uint8Array>;
 }
 
-class ZipEntryReadableStream extends ReadableStream<Uint8Array> {
-  #reader: ByobTransformStream;
+class BufferReader {
+  #buffer: Uint8Array;
 
-  #compressedSize: number;
+  #reader: ReadableStreamDefaultReader<Uint8Array>;
 
-  #count: number;
+  #open: boolean;
 
-  constructor(reader: ByobTransformStream, compressedSize: number) {
-    super({
-      pull: async (controller) => {
-        const chunk = await this.#read();
-        controller.enqueue(chunk);
-        if (this.consumed) {
-          controller.close();
-        }
-      },
-      cancel: async () => {
-        while (this.consumed) {
-          await this.#read();
-        }
-      },
-    });
-    this.#reader = reader;
-    this.#compressedSize = compressedSize;
-    this.#count = 0;
+  constructor(readable: ReadableStream<Uint8Array>) {
+    this.#buffer = new Uint8Array(0);
+    this.#reader = readable.getReader();
+    this.#open = true;
   }
 
-  async #read(): Promise<Uint8Array<ArrayBufferLike> | undefined> {
-    if (this.consumed) {
-      return undefined;
+  cancel(reason: any) {
+    this.#open = false;
+    return this.#reader.cancel(reason);
+  }
+
+  async readNBytes(length: number): Promise<Uint8Array> {
+    const b = new Uint8Array(length);
+    return b.subarray(0, await this.read(b));
+  }
+
+  async read<T extends ArrayBufferLike>(
+    destination: Uint8Array<T>,
+  ): Promise<number> {
+    let offset = 0;
+    while (this.#open) {
+      const r = this.#buffer.subarray(
+        0,
+        Math.min(destination.byteLength - offset, this.#buffer.byteLength),
+      );
+      destination.set(r, offset);
+      offset += r.byteLength;
+      this.#buffer = this.#buffer.subarray(r.byteLength);
+      if (offset === destination.length) {
+        break;
+      }
+      // Wait for data
+      const done = await this.#wait();
+      if (done === null) {
+        break;
+      }
     }
-    const chunk = await this.#reader.read(Math.min(this.remaning, 16_640));
-    this.#count += chunk.byteLength;
-    return chunk;
+    return offset;
   }
 
-  get remaning(): number {
-    return this.#compressedSize - this.#count;
+  slice(dest: Uint8Array): number {
+    const b = this.#buffer.subarray(0, Math.min(this.#buffer.byteLength, dest.byteLength));
+    dest.set(b);
+    this.#buffer = this.#buffer.subarray(b.byteLength);
+    return b.byteLength;
   }
 
-  get consumed(): boolean {
-    return this.remaning <= 0;
+  async #wait(): Promise<void> {
+    const { value, done } = await this.#reader.read();
+    if (done) {
+      this.#open = false;
+      return;
+    }
+    this.#buffer = value;
   }
 }
 
-function getZipStream(
-  // reader: ReadableStreamBYOBReader,
-  reader: ByobTransformStream,
-  { compressedSize, compression }: LocalFileHeader,
-): ReadableStream<Uint8Array> {
-  if (compressedSize === 0) {
-    return new ReadableStream<Uint8Array>({
-      pull: (controller) => controller.close(),
-    });
-  }
-  let stream: ReadableStream<Uint8Array> = new ZipEntryReadableStream(
-    reader,
-    compressedSize,
-  );
-  if (compression === Compression.DEFLATED) {
-    stream = stream.pipeThrough(new DecompressionStream("deflate-raw"));
-  }
-  return stream;
-}
 
-async function* unzip(
-  readable: ReadableStream<Uint8Array>,
-): AsyncGenerator<ZipEntry> {
-  const decoder = new TextDecoder();
-  const reader = new ByobTransformStream(readable);
-  let headerBuffer = await reader.read(30);
-  if (!startsWith(headerBuffer, LOCAL_FILE_SIGNATURE)) {
-    await reader.cancel();
-    throw new Error("Invalid zip");
+class UnzipTransformStream implements TransformStream<Uint8Array, ZipEntry> {
+  #queue: BufferReader;
+
+  #decoder: TextDecoder;
+
+  #readable: ReadableStream<ZipEntry>;
+
+  #writable: WritableStream<Uint8Array>;
+
+  get readable(): ReadableStream<ZipEntry> {
+    return this.#readable;
   }
-  do {
-    const header = createHeader(headerBuffer);
-    const filename = await reader.read(header.filenameLength);
-    // TODO: Parse extra fields
-    await reader.read(header.extraFieldsLength);
-    const readable = getZipStream(reader, header);
-    yield {
-      filename: decoder.decode(filename),
-      header,
-      headerBuffer,
-      readable,
+
+  get writable(): WritableStream<Uint8Array> {
+    return this.#writable;
+  }
+
+  constructor() {
+    const { readable, writable } = new TransformStream<
+      Uint8Array,
+      Uint8Array
+    >();
+    this.#decoder = new TextDecoder();
+    this.#queue = new BufferReader(readable);
+    this.#readable = ReadableStream.from(this.#unzip());
+    this.#writable = writable;
+  }
+
+  async *#unzip(): AsyncGenerator<ZipEntry> {
+    let buf: Uint8Array;
+    for (;;) {
+      buf = await this.#queue.readNBytes(30);
+      if (buf.byteLength !== 30 || !startsWith(buf, LOCAL_FILE_SIGNATURE)) {
+        break;
+      }
+      const {
+        compressedSize,
+        compression,
+        crc32,
+        extraFieldsLength,
+        filenameLength,
+        flags,
+        date,
+        size,
+        version,
+      } = this.#createHeader(buf);
+      const expectedLength = filenameLength + extraFieldsLength;
+      buf = await this.#queue.readNBytes(expectedLength);
+      if (buf.byteLength !== expectedLength) {
+        throw new Error("Cannot read filename");
+      }
+      const filename = this.#decoder.decode(buf.subarray(0, filenameLength));
+      const extraFields = this.#parseExtra(
+        buf.buffer,
+        filenameLength,
+        extraFieldsLength,
+      );
+      let entryReadable = ReadableStream.from(this.#readEntryContent(compressedSize));
+      if (compression === Compression.DEFLATED) {
+        entryReadable = entryReadable.pipeThrough(
+          new DecompressionStream("deflate-raw")
+        );
+      } else if (compression !== Compression.RAW) {
+        console.warn("Unknown compression:", compression);
+      }
+      yield {
+        header: {
+          compressedSize,
+          compression,
+          crc32: 0,
+          extraFields,
+          filename,
+          flags,
+          date: new Date(),
+          size,
+          version,
+        },
+        readable: entryReadable,
+      };
+    }
+  }
+
+  async *#readEntryContent(compressedSize: number): AsyncGenerator<Uint8Array> {
+    if (compressedSize === 0) {
+      return;
+    }
+    const buffer = new ArrayBuffer(Math.min(compressedSize, 16_192));
+    let count = this.#queue.slice(new Uint8Array(buffer));
+    for (;;) {
+      yield new Uint8Array(buffer, 0, count);
+      compressedSize -= count;
+      if (compressedSize === 0) {
+        break;
+      }
+      count = await this.#queue.read(new Uint8Array(buffer, 0, Math.min(compressedSize, buffer.byteLength)));
+    }
+  }
+
+  #parseExtra(
+    buf: ArrayBufferLike,
+    offset: number,
+    length: number,
+  ): [number, number][] {
+    if (length === 0) {
+      return [];
+    }
+    const extra: [number, number][] = [];
+    const view = new Uint16Array(buf, offset, length);
+    let current: number[] = [];
+    for (const n of view) {
+      if (current.push(n) === 2) {
+        extra.push(current as [number, number]);
+        current = [];
+      }
+    }
+    return extra;
+  }
+
+  #createHeader(
+    { buffer, byteOffset }: ArrayBufferView,
+  ): InternalLocalFileHeader {
+    const view = new DataView(buffer, byteOffset + LOCAL_FILE_SIGNATURE.byteLength);
+    const dostime = view.getUint32(6, true);
+    const year = ((dostime >> 25) & 0x7f) + 1980;
+    const month = ((dostime >> 21) & 0x0f);
+    const day = ((dostime >> 16) & 0x1f)
+    const hour = ((dostime >> 11) & 0x1f);
+    const minute = ((dostime >> 5) & 0x3f);
+    const second = ((dostime << 1) & 0x3e);
+    let date: Date | undefined;
+    if (month > 0 && month < 13 && day > 0 && hour < 24 && minute < 60 && second < 60) {
+      date = new Date(Date.UTC(year, month - 1, day, hour, minute, second));
+    }
+    return {
+      version: view.getUint16(0, true),
+      flags: view.getUint16(2, true),
+      compression: view.getUint16(4, true),
+      // view.getUint32(10, true)
+      crc32: Uint8Array.of(
+        view.getUint8(10),
+        view.getUint8(11),
+        view.getUint8(13),
+      ),
+      compressedSize: view.getUint32(14, true),
+      size: view.getUint32(18, true),
+      filenameLength: view.getUint16(22, true),
+      extraFieldsLength: view.getUint16(24, true),
+      date,
     };
-    headerBuffer = await reader.read(headerBuffer.byteLength);
-  } while (startsWith(headerBuffer, LOCAL_FILE_SIGNATURE));
-  await reader.cancel();
+  }
 }
 
 interface MainArgs {
@@ -215,10 +273,12 @@ async function main(args: string[]): Promise<number> {
     return 1;
   }
   const { infile, outdir } = await prepareArgs(args);
-  for await (const options of unzip(infile.readable)) {
-    const outpath = stdPath.join(outdir, options.filename);
-    console.log(options.filename, "=>", outpath);
-    if (options.filename.endsWith("/")) {
+  for await (
+    const { header, readable } of infile.readable.pipeThrough(new UnzipTransformStream())
+  ) {
+    const outpath = stdPath.join(outdir, header.filename);
+    console.log(header.filename, "=>", outpath);
+    if (header.filename.endsWith("/")) {
       await Deno.mkdir(outpath);
       continue;
     }
@@ -226,8 +286,7 @@ async function main(args: string[]): Promise<number> {
       write: true,
       createNew: true,
     });
-    await options.readable.pipeTo(outfile.writable);
-    // await options.content.pipeTo(Deno.stdout.writable, { preventClose: true });
+    await readable.pipeTo(outfile.writable);
   }
   return 0;
 }
